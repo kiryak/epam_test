9adb139baba3c991de72303d3b0cf5ff3b5f927c
Mariia Palyga <kiryakovam@gmail.comgit  config --global user.name  >
2019-09-01 08:31:42 +0400

    4th Commit

--- Task4/Curriculum/JFC_String.java
+++ Task4/Curriculum/JFC_String.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 public class JFC_String {
-    private Duration duration = Duration.of(16/8, ChronoUnit.DAYS);
+    private Duration duration = Duration.of(16, ChronoUnit.HOURS);
 
     public Duration getDuration() {
         return duration;
--- Task4/Curriculum/JavaTechnologies.java
+++ Task4/Curriculum/JavaTechnologies.java
@@ -4,7 +4,7 @@ import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 public class JavaTechnologies {
-    private Duration duration = Duration.of(8/8, ChronoUnit.DAYS);
+    private Duration duration = Duration.of(8, ChronoUnit.HOURS);
 
     public Duration getDuration() {
         return duration;
